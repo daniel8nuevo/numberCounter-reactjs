4ec34bac7e0c42850ae10b9b64d9ad3a50b8259e
daniel8nuevo <nuevo8vida@gmail.com>
2020-05-12 17:38:45 +0300

    added NumberCounter component and handleClickPlus method in App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,36 @@
 import React from 'react';
 import './App.css';
 
+export default class NumberCounter extends React.Component {
+  constructor(props){
+    super(props);
+    this.handleClickPlus = this.handleClickPlus.bind(this);
+    this.state = {
+      count: 0
+    }
+  }
 
+  handleClickPlus() {
+    this.setState({
+      count : this.state.count + 1
+    });
+  }
+
+  render(){
+    return(
+      <div className='counter'>
+
+        <span>
+          {this.state.count}
+        </span>
+
+        <br />
+
+        <button>-</button>
+        <button onClick={this.handleClickPlus}>+</button>
+        
+
+      </div>
+    );
+  }
+}
